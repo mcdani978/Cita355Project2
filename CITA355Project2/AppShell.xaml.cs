b3a317019f39b19bba9f77cdf730cbd852d1495a
dmccowan <mccowa206@morrisville.edu>
2025-04-24 12:12:14 -0400

    Project create

---
+++ CITA355Project2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CITA355Project2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CITA355Project2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CITA355Project2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
