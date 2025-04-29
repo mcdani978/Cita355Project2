e497d17a51dae1bf6b4c04f60e4468d076de564b
mcdani978 <mcdani978@morrisville.edu>
2025-04-29 12:34:27 -0400

    Some Functionality Added To Student Sign Up Page
    just added in some fields for the student to enter their their name, email, and password. More will be added later.

--- CITA355Project2/StudentSignupPage.xaml.cs
+++ CITA355Project2/StudentSignupPage.xaml.cs
@@ -6,4 +6,15 @@ public partial class StudentSignupPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnSubmitClicked(object sender, EventArgs e)
+    {
+        string username = usernameEntry.Text;
+        string firstName = firstNameEntry.Text;
+        string lastName = lastNameEntry.Text;
+        string password = passwordEntry.Text;
+
+        // For now, just display the data in an alert
+        DisplayAlert("Signup Info", $"Username: {username}\nFirst Name: {firstName}\nLast Name: {lastName}\nPassword: {password}", "OK");
+    }
 }
